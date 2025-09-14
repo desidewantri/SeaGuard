64a9902e93f9cf85f7de542daefac49f67542530
Hayunitya <hayunitya549@gmail.com>
2025-09-14 20:47:35 +0700

    Add ClassSeaGuardApp

---
+++ ClassSeaGuardApp/Enum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSeaGuardApp
+{
+    // Status validasi laporan sampah
+    public enum StatusValidasi
+    {
+        Menunggu,
+        Valid,
+        TidakValid
+    }
+
+    // Kategori sampah
+    public enum KategoriSampah
+    {
+        Plastik,
+        Logam,
+        Kaca,
+        Organik,
+        Lainnya
+    }
+}
